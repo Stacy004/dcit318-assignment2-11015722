35e83067c6d8b7d7b15c520fc7273ff7b10e3058
Stacy004 <aaaandoh@st.ug.edu.gh>
2025-08-07 11:13:20 +0000

    Initial commit with Inheritance and Method Overriding

---
+++ assignment 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Assignmet2
+{
+    public class Animal 
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+
+    public class Dog : Animal 
+    {
+       public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();    
+
+
+            Console.WriteLine("Animal sound:");
+            myAnimal.MakeSound();
+
+            Console.WriteLine("\nDog sound:");
+            dog.MakeSound();
+
+            Console.WriteLine("\nCat sound:");
+            cat.MakeSound();
+        }
+    }
+
+
+}
+
+
+
